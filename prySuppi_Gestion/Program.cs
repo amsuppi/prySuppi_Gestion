5486b347a4e30f51f1d2117017381d9cf6d6d7c9
amsuppi <maribelsuppi97@gmail.com>
2025-05-16 18:44:12 -0300

    Nuevos cambios de login

--- prySuppi_Gestion/Program.cs
+++ prySuppi_Gestion/Program.cs
@@ -16,7 +16,7 @@ namespace prySuppi_Gestion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmGestion());
         }
     }
 }
